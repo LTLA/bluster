db0457bb6a30337b6747cce677816a93ef21d3f6
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-01-09 18:37:50 -0800

    Support affinity propagation via the AffinityParam class.

---
+++ R/AffinityParam.R
@@ -0,0 +1,126 @@
+#' Affinity propogation 
+#'
+#' Use affinity propagation from the \pkg{apcluster} package to cluster observations.
+#' Note that this requires the installation of the \pkg{apcluster} package.
+#'
+#' @inheritParams clusterRows
+#' @param s A function that accepts a matrix of observations by dimensions and returns a similarity matrix.
+#' If \code{NULL}, defaults to the output of \code{\link[apcluster]{negDistMat}} with \code{r=2}.
+#' @param p,q Numeric scalars controlling the input preference, i.e., the resolution of the clustering.
+#' These are passed to the \code{\link[apcluster]{apcluster}} function, where values of \code{NA} are the default.
+#' @param maxits,convits,lam,nonoise
+#' Further arguments to pass to the \code{\link[apcluster]{apcluster}} function.
+#' @param BLUSPARAM A \linkS4class{AffinityParam} object.
+#' @param full Logical scalar indicating whether the full affinity propagation statistics should be returned.
+#' 
+#' @details
+#' To modify an existing AffinityParam object \code{x},
+#' users can simply call \code{x[[i]]} or \code{x[[i]] <- value} where \code{i} is any argument used in the constructor.
+#'
+#' Setting \code{q} (and less typically, \code{p}) allows us to tune the resolution of the clustering.
+#' Larger values yield more clusters as each data point is more inclined to form its own cluster.
+#' Any choice for \code{q} should lie in [0, 1] (the default is 0.5 when \code{q=NA}).
+#' 
+#' @return
+#' The \code{AffinityParam} constructor will return a \linkS4class{AffinityParam} object with the specified parameters.
+#'
+#' The \code{clusterRows} method will return a factor of length equal to \code{nrow(x)} containing the cluster assignments.
+#' If \code{full=TRUE}, a list is returned with \code{clusters} (the factor, as above) and \code{objects};
+#' the latter will contain the direct output of \code{\link[apcluster]{apcluster}}.
+#'
+#' @author Aaron Lun
+#' @examples
+#' clusterRows(iris[,1:4], AffinityParam())
+#' clusterRows(iris[,1:4], AffinityParam(q=0.9))
+#' clusterRows(iris[,1:4], AffinityParam(s=apcluster::expSimMat()))
+#'
+#' @seealso
+#' \code{\link[apcluster]{apcluster}} from the \pkg{apcluster} package, which does all of the heavy lifting.
+#'
+#' @name AffinityParam-class
+#' @docType class
+#' @aliases 
+#' show,AffinityParam-method
+NULL
+
+#' @export
+setClass("AffinityParam", contains="BlusterParam", slots=c(s="function_OR_NULL",
+    p="numeric", q="numeric", maxits="integer", convits="integer", lam="numeric", nonoise="logical"))
+
+#' @export
+#' @rdname AffinityParam-class
+AffinityParam <- function(s=NULL, p=NA, q=NA, maxits=1000, convits=100, lam=0.9, nonoise=FALSE) {
+    new("AffinityParam", s=s, p=as.numeric(p), q=as.numeric(q), 
+        maxits=as.integer(maxits), convits=as.integer(convits), 
+        lam=as.numeric(lam), nonoise=as.logical(nonoise))
+}
+
+#' @export
+setMethod("show", "AffinityParam", function(object) {
+    callNextMethod()
+
+    cat(sprintf("s: %s\n", if (is.null(object[["s"]])) "default" else "custom"))
+    for (i in c("p", "q", "maxits", "convits", "lam", "nonoise")) {
+        cat(sprintf("%s: %s\n", i, slot(object, i)))
+    }
+})
+
+setValidity2("AffinityParam", function(object) {
+    msg <- .check_positive_slots(object, c("maxits", "convits"))
+    
+    msg <- .check_nonna_slots(object, c("nonoise", "lam"))
+
+    if (object@lam >= 1 || object@lam < 0.5) {
+        msg <- c(msg, "'lam' should be in [0.5, 1)")
+    }
+
+    if (length(object[["p"]])!=1) {
+        msg <- c(msg, "'p' should be of length 1")
+    }
+
+    q <- object[["q"]]
+    if (length(q)!=1) {
+        msg <- c(msg, "'q' should be of length 1")
+    } else {
+        if (!is.na(q) && (q < 0 || q > 1)) {
+            msg <- c("'q' should lie in [0, 1]")
+        }
+    }
+
+    if (object@lam >= 1 || object@lam < 0.5) {
+        msg <- c(msg, "'lam' should be in [0.5, 1)")
+    }
+
+    if (length(msg)) {
+        return(msg)
+    }
+    TRUE
+})
+
+#' @export
+#' @rdname AffinityParam-class
+setMethod("clusterRows", c("ANY", "AffinityParam"), function(x, BLUSPARAM, full=FALSE) {
+    s <- BLUSPARAM[["s"]]
+    if (is.null(s)) {
+        s <- apcluster::negDistMat(r=2)
+    }
+
+    res <- apcluster::apcluster(s=s, 
+        x=as.matrix(x), 
+        p=BLUSPARAM[["p"]],
+        q=BLUSPARAM[["q"]],
+        maxits=BLUSPARAM[["maxits"]],
+        convits=BLUSPARAM[["convits"]],
+        lam=BLUSPARAM[["lam"]],
+        nonoise=BLUSPARAM[["nonoise"]]
+    )
+
+    out <- apcluster::labels(res, type="enum")
+    out <- factor(out)
+
+    if (full) {
+        list(clusters=out, objects=res)
+    } else {
+        out
+    }
+})
---
+++ tests/testthat/test-affinity-param.R
@@ -0,0 +1,61 @@
+# Tests the AffinityParam class.
+# library(bluster); library(testthat); source('test-affinity-param.R')
+
+set.seed(1000)
+test_that("AffinityParam constructor and utilities work correctly", {
+    X <- AffinityParam()
+    expect_output(show(X), "AffinityParam")
+    expect_output(show(X), "s: default")
+    expect_output(show(X), "p: NA")
+
+    expect_identical(X[["s"]], NULL)
+    X[["s"]] <- identity
+    expect_identical(X[["s"]], identity)
+
+    X <- AffinityParam(q=0.9)
+    expect_identical(X[["q"]], 0.9)
+    X[["q"]] <- 0.1
+    expect_identical(X[["q"]], 0.1)
+})
+
+test_that("clusterRows works correctly", {
+    m <- matrix(runif(1000), ncol=10)
+
+    out <- clusterRows(m, AffinityParam())
+    expect_true(is.factor(out))
+    expect_identical(length(out), nrow(m))
+
+    # Consistent with reference values.
+    ref <- apcluster::apcluster(apcluster::negDistMat(m, r=2))
+    expect_identical(out, factor(apcluster::labels(ref, type="enum")))
+
+    # Trying with different parameters.
+    set.seed(10)
+    out <- clusterRows(m, AffinityParam(q=0.8))
+    expect_true(is.factor(out))
+    expect_identical(length(out), nrow(m))
+
+    set.seed(10)
+    ref <- apcluster::apcluster(apcluster::negDistMat(m, r=2), q=0.8)
+    expect_identical(out, factor(apcluster::labels(ref, type="enum")))
+})
+
+test_that("clusterRows responds to full=TRUE", {
+    m <- matrix(runif(2960), ncol=10)
+    out <- clusterRows(m, AffinityParam())
+
+    full <- clusterRows(m, AffinityParam(), full=TRUE)
+    expect_identical(out, full$cluster)
+    expect_s4_class(full$objects, "APResult")
+})
+
+test_that("clusterRows responds to the options", {
+    m <- matrix(runif(1000), ncol=10)
+
+    set.seed(1000)
+    out <- clusterRows(m, AffinityParam(q=0.8, lam=0.7, s=apcluster::expSimMat()))
+
+    set.seed(1000)
+    ref <- apcluster::apcluster(apcluster::expSimMat(m), q=0.8, lam=0.7)
+    expect_identical(out, factor(apcluster::labels(ref, type="enum")))
+})
